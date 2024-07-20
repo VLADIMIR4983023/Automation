b421430aeeb7b35dadaf4588470a17bd6e55180e
Vladimir <154542129+VLADIMIR4983023@users.noreply.github.com>
2024-07-20 18:56:43 +0300

    Lesson7

---
+++ Lesson7/homework/Task1/form_page_obect_test.py
@@ -0,0 +1,57 @@
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from pages.MainPage import MainPage
+from pages.ResultPage import ResultPage
+
+
+def test_red_element():
+    browser = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()))
+    browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    main_page = MainPage(browser)
+
+    main_page.first_name("Иван")
+    main_page.last_name("Петров")
+    main_page.address("Ленина, 55-3")
+    main_page.email("test@skypro.com")
+    main_page.phone("+7985899998787")
+    main_page.zip_code("")
+    main_page.city("Москва")
+    main_page.country("Россия")
+    main_page.job_position("QA")
+    main_page.company("SkyPro")
+    main_page.send_form()
+
+    result_page = ResultPage(browser)
+    to_be = result_page.empty_zip_result_is_red()
+
+    as_is = 1
+
+    assert as_is == to_be
+
+    browser.quit()
+
+
+def test_green_elements():
+    browser = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()))
+    main_page = MainPage(browser)
+
+    main_page.first_name("Иван")
+    main_page.last_name("Петров")
+    main_page.address("Ленина, 55-3")
+    main_page.email("test@skypro.com")
+    main_page.phone("+7985899998787")
+    main_page.zip_code("")
+    main_page.city("Москва")
+    main_page.country("Россия")
+    main_page.job_position("QA")
+    main_page.company("SkyPro")
+    main_page.send_form()
+
+    result_page = ResultPage(browser)
+    classes = result_page.other_elements_result_is_green()
+
+    assert 'alert-success' in classes
